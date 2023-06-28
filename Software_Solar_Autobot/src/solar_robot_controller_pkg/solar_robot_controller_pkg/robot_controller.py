ccc6770198ad9cbfeaf98536951748c10cbeb80c
Tarunesh38 <119646778+Tarunesh38@users.noreply.github.com>
2023-06-29 01:24:27 +0530

    Update robot_controller.py

--- Software_Solar_Autobot/src/solar_robot_controller_pkg/solar_robot_controller_pkg/robot_controller.py
+++ Software_Solar_Autobot/src/solar_robot_controller_pkg/solar_robot_controller_pkg/robot_controller.py
@@ -1,7 +1,3 @@
-# Author: Your Name
-# Date: June 27, 2023
-# ROS Version: ROS 2 Foxy Fitzroy
-
 ############## IMPORT LIBRARIES #################
 # Python math library
 import math
@@ -39,6 +35,13 @@ from rclpy.qos import qos_profile_sensor_data
 # Scientific computing library
 import numpy as np
 
+# Import OpenCV
+import cv2
+import numpy as np
+
+# Import additional ROS message types
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 class Controller(Node):
     """
@@ -69,6 +72,27 @@ class Controller(Node):
 
         # Start moving the robot randomly
         self.move_randomly()
+
+        ##################### LIDAR SETUP #############################
+        # Create a subscriber
+        # This node subscribes to the LaserScan data from the /demo/laser_scan topic.
+        self.laser_subscriber_ = self.create_subscription(
+            LaserScan,
+            '/demo/laser_scan',
+            self.laser_callback,
+            qos_profile=qos_profile_sensor_data)
+
+        ##################### CAMERA SETUP ############################
+        # Create a subscriber
+        # This node subscribes to the Image data from the /demo/camera/image_raw topic.
+        self.image_subscriber_ = self.create_subscription(
+            Image,
+            '/demo/camera/image_raw',
+            self.image_callback,
+            qos_profile=10)
+
+        # Create an instance of CvBridge
+        self.cv_bridge = CvBridge()
 
     def move_randomly(self):
         """
@@ -109,43 +133,68 @@ class Controller(Node):
         # Publish the velocity command to the robot
         self.publisher_.publish(msg)
 
+    def laser_callback(self, msg):
+        """
+        Callback function for the LaserScan data
+        """
+        # Process the LaserScan data to avoid obstacles
+        # ... Add your collision avoidance logic here ...
+
+        # Stop the robot if an obstacle is too close
+        if min(msg.ranges) < 0.5:
+            self.stop_robot()
+
+    def image_callback(self, msg):
+        """
+        Callback function for the Image data
+        """
+        # Convert the Image message to OpenCV format
+        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        # Process the image data to follow the white light source
+        # ... Add your color following logic here ...
+
+    def main(self):
+        """
+        Main function to control the robot
+        """
+        while True:
+            # Move straight for 15 seconds
+            sleep(5)
+
+            # Stop the robot's movement
+            self.stop_robot()
+
+            # Rotate randomly to the left or right
+            if random.random() < 0.5:
+                # Rotate to the left
+                self.move_randomly()
+            else:
+                # Rotate to the right
+                self.move_randomly()
+
+            # Keep rotating for 2 seconds
+            sleep(2)
+
+            # Stop the robot's movement
+            self.stop_robot()
+
+            # Move straight again
+            self.move_randomly()
 
 def main(args=None):
-
     # Initialize rclpy library
     rclpy.init(args=args)
 
     # Create the node
     controller = Controller()
 
-    while True:
-        # Move straight for 15 seconds
-        sleep(15)
-
-        # Stop the robot's movement
-        controller.stop_robot()
-
-        # Rotate randomly to the left or right
-        if random.random() < 0.5:
-            # Rotate to the left
-            controller.move_randomly()
-        else:
-            # Rotate to the right
-            controller.move_randomly()
-
-        # Keep rotating for 5 seconds
-        sleep(5)
-
-        # Stop the robot's movement
-        controller.stop_robot()
-
-        # Move straight again
-        controller.move_randomly()
-
-    # Spin the node so the callback function is called
-    # Pull messages from any topics this node is subscribed to
-    # Publish any pending messages to the topics
-    rclpy.spin(controller)
+    try:
+        # Run the main loop
+        controller.main()
+    except KeyboardInterrupt:
+        # Handle Ctrl+C interruption
+        pass
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
@@ -158,4 +207,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
